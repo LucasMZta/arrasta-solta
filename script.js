5da13ea52b28cdb1134be3ae3af624d0ef079cf0
Lucas Martins <lucas2zonta@hotmail.com>
2023-02-10 13:50:00 -0300

    Iniciando o repositorio

---
+++ script.js
@@ -0,0 +1,14 @@
+document.querySelectorAll('.item').forEach((item)=>{
+    item.addEventListener('dragstart', dragStart);
+    item.addEventListener('dragend', dragEnd);
+});
+
+//Function Item
+function dragStart(e) {
+    e.currentTarget.classList.add('dragging');
+}
+function dragEnd(e) {
+    e.currentTarget.classList.remove('dragging');
+}
+
+//Functions Area
