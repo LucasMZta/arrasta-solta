c5fe65566c1f62838146596f8435f72ff9777034
Lucas Martins <lucas2zonta@hotmail.com>
2023-02-10 16:21:28 -0300

    Atualizando a sequencia do drop para ficar verde

--- readme.md
+++ readme.md
@@ -1,3 +1,4 @@
 # Projeto 7 - Drag and Drop
 
 Utilizando dos elementos 1, 2 e 3 disponívens, é possivel reposiciona-los nas areas destacadas disponiveis. Ao reordenar elas novamente, toda a area passa a ter a borda verde.
+Ao arrastar os itens para a area debaixo e ficarem sequenciais novamente (ordenado por 1, 2 e 3), a area em volta ficará com a borda verde;
--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let seqAreas = {a: null, b: null, c: null};
+
 document.querySelectorAll('.item').forEach((item) => {
     item.addEventListener('dragstart', dragStart);
     item.addEventListener('dragend', dragEnd);
@@ -7,7 +9,9 @@ document.querySelectorAll('.area').forEach((area) => {
     area.addEventListener('dragleave', dragLeave);
     area.addEventListener('drop', drop);
 });
-
+document.querySelector('.neutralArea').addEventListener('dragover', dragOverNeutral);
+document.querySelector('.neutralArea').addEventListener('dragleave', dragLeaveNeutral);
+document.querySelector('.neutralArea').addEventListener('drop', drogNeutral);
 //Function Item
 function dragStart(e) {
     e.currentTarget.classList.add('dragging');
@@ -18,17 +22,52 @@ function dragEnd(e) {
 
 //Functions Area
 function dragOver(e) {
-    if(e.currentTarget.innerHTML === '') {
+    if(e.currentTarget.querySelector('.item') === null) {
         e.preventDefault();
+        e.currentTarget.classList.add('hover');
     }
-    e.currentTarget.classList.add('hover');
 }
 function dragLeave(e) {
     e.currentTarget.classList.remove('hover');
 }
 function drop(e) {
     e.currentTarget.classList.remove('hover');
+    if(e.currentTarget.querySelector('.item') === null) {
+        let dragItem = document.querySelector('.item.dragging');
+        e.currentTarget.appendChild(dragItem);
+        updateAreas()
+    } 
+}
 
+//Functions Neutral Area
+function dragOverNeutral(e) {
+    e.preventDefault();
+    e.currentTarget.classList.add('hover');
+}
+function dragLeaveNeutral(e) {
+    e.currentTarget.classList.remove('hover');
+}
+function drogNeutral(e) {
+    e.currentTarget.classList.remove('hover');
     let dragItem = document.querySelector('.item.dragging');
     e.currentTarget.appendChild(dragItem);
+    updateAreas()
+} 
+
+//Logic Functions
+function updateAreas() {
+    document.querySelectorAll('.area').forEach((area)=>{
+        let name = area.getAttribute('data-name');
+        if(area.querySelector('.item') !== null) {
+            seqAreas[name] = area.querySelector('.item').innerHTML;
+        } else {
+            seqAreas[name] = null;
+        }
+    });
+
+    if(seqAreas.a == '1' && seqAreas.b == '2' && seqAreas.c == '3') {
+        document.querySelector('.areas').classList.add('correct');
+    }  else {
+        document.querySelector('.areas').classList.remove('correct');
+    }
 }
